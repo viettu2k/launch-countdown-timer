c6bc3260d457679bd2548874e7564150a07650fe
viettu2k <doanviettu2k@gmail.com>
2022-05-17 20:57:47 +0700

    finish

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,65 @@ import { TimerContainer } from '../components/TimerContainer'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
 import { TimerInput } from '../components/TimerInput'
+import { useEffect, useState } from 'react'
 
 const Home: NextPage = () => {
+  const [newTime, setNewTime] = useState<number>(0)
+  const [time, setTime] = useState<number>(0)
+  const [days, setDays] = useState<number>(0)
+  const [hours, setHours] = useState<number>(0)
+  const [minutes, setMinutes] = useState<number>(0)
+  const [seconds, setSeconds] = useState<number>(0)
+  const [message, setMessage] = useState<string>('')
+
+  const timeToDays = time * 60 * 60 * 24 * 1000
+
+  let countDownDate = new Date().getTime() + timeToDays
+
+  useEffect(() => {
+    var updateTime = setInterval(() => {
+      var now = new Date().getTime()
+
+      var difference = countDownDate - now
+
+      var newDays = Math.floor(difference / (1000 * 60 * 60 * 24))
+      var newHours = Math.floor(
+        (difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      )
+      var newMinutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
+      var newSeconds = Math.floor((difference % (1000 * 60)) / 1000)
+
+      setDays(newDays)
+      setHours(newHours)
+      setMinutes(newMinutes)
+      setSeconds(newSeconds)
+
+      if (difference <= 0) {
+        clearInterval(updateTime)
+        setMessage('The Launch Has Started')
+        setDays(0)
+        setHours(0)
+        setMinutes(0)
+        setSeconds(0)
+      }
+    })
+
+    return () => {
+      clearInterval(updateTime)
+    }
+  }, [time])
+
+  const handleClick = () => {
+    setTime(newTime)
+    console.log(time)
+    setNewTime(0)
+  }
+
+  const handleChange = (e: any) => {
+    let inputTime = e.target.value
+    setNewTime(inputTime)
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center bg-[#1e1f29]">
       <Head>
